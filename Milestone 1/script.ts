a9d5a5c97740731b97a0586ed2c1b407c846b0a8
Syeda Aqsa Hassan Sami <166224660+SyedaAqsaHassanSami674@users.noreply.github.com>
2024-10-18 14:17:49 +0500

    Add files via upload

---
+++ Milestone 1/script.ts
@@ -0,0 +1,9 @@
+const toggleSkills = document.getElementById("toggle skills") as HTMLButtonElement
+const skills= document.getElementById("skills") as HTMLElement
+toggleSkills.addEventListener("click",()=>{
+    if (skills.style.display === "none") {
+        skills.style.display = "block"
+        }else{
+        skills.style.display = "none"
+    }
+});
